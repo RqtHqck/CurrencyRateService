7c87de885a170e6fee609c32c67bf9ba509a64ca
Ihar <rathack13@gmail.com>
2024-11-03 23:48:19 +0300

    Api route /getCoupleCurrency for currencies exchange completed. Validation for query return callback fn, some logical change. New method for CurrencyController.js transferCurrency for exchanging and check validation for Db request with today API data

--- middlewares/ValidateQueryParams.js
+++ middlewares/ValidateQueryParams.js
@@ -1,35 +1,37 @@
 const { query, validationResult} = require('express-validator');
+const logger = require('../utils/logHandler');
 
 
-module.exports = class ValidateQueryParams {
-  static validateDate = [
-    query('date', 'Incorrect value')
-      .optional() // unnecessary query param
-      .isISO8601().withMessage('Date have to be in format YYYY-MM-DD'),
+class ValidateQueryParams {
+  static validateDate = () => {
+    return [
+      query('date', 'Incorrect value')
+        .optional() // unnecessary query param
+        .isISO8601().withMessage('Date have to be in format YYYY-MM-DD'),
+      this.handleValidationErrors
+    ];
+  }
 
-    (req, res, next) => {
-      const errors = validationResult(req); // Collect all errors from validation chain
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-      next();
+  static validateTicketName = () => {
+    return [
+      query(['ticket', 'from', 'to'], 'Incorrect value')
+        .optional()
+        .isString()
+        .isIn(['EUR', 'BYN', 'RUB', 'JPY', 'KZT', 'CNY', 'AUD', 'GBP'])
+        .withMessage('Ticket must be one of: EUR, BYN, RUB, JPY, KZT, CNY, AUD, GBP'),
+      this.handleValidationErrors
+    ]
+  }
+
+  static handleValidationErrors = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      logger.info('Validation errors:', errors.array()); // Добавлено логирование
+      return res.status(400).json({ errors: errors.array() });
     }
-  ];
+    next();
+  }
 
-  static validateTicketName = [
-    query('ticket', 'Incorrect value')
-      .optional()
-      .isString()
-      .isIn(['EUR', 'BYN', 'RUB', 'JPY', 'KZT', 'CNY', 'AUD', 'GBP'])
-      .withMessage('Ticket must be one of: EUR, BYN, RUB, JPY, KZT, CNY, AUD, GBP'),
-
-    (req, res, next) => {
-      const errors = validationResult(req); // Collect all errors from validation chain
-      if (!errors.isEmpty()) {
-        // Обработать ошибку квери параметра
-        return res.status(400).json({ errors: errors.array() });
-      }
-      next();
-    }
-  ]
 }
+
+module.exports = ValidateQueryParams;
--- routes/currency.js
+++ routes/currency.js
@@ -2,16 +2,14 @@ const router = require('express').Router();
 
 const CurrencyController = require('../controllers/CurrencyController')
 const ValidateQueryParams = require('../middlewares/ValidateQueryParams')
-const currency = new CurrencyController();
 
+// GET http://localhost:5000/api/currency/getCurrencies?date=date&ticket=ticket
+router.get('/getCurrencies', ValidateQueryParams.validateDate(), ValidateQueryParams.validateTicketName(), CurrencyController.getCurrencies)
 
-// GET http://localhost:5000/api/currency/getAllCurrenciesByDate?date=date&ticket=ticket
-router.get('/getAllCurrencies', ValidateQueryParams.validateDate, ValidateQueryParams.validateTicketName, currency.getAllCurrencies)
-
-// GET http://localhost:5000/api/currency/getCoupleCurrency?date=date&from=from&to=to
-router.get('/getCoupleCurrency', ValidateQueryParams.validateDate, ValidateQueryParams.validateTicketName, currency.getCoupleCurrency)
+// GET http://localhost:5000/api/currency/getCoupleCurrency?from=from&to=to
+router.get('/getCoupleCurrency', ValidateQueryParams.validateTicketName(), CurrencyController.getCoupleCurrency)
 
 // GET http://localhost:5000/api/currency/saveCurrencyFreaks
-router.get('/saveCurrencyFreaks', currency.saveCurrencyFreaks)
+router.get('/saveCurrencyFreaks', CurrencyController.saveCurrencyFreaks)
 
 module.exports = router;
